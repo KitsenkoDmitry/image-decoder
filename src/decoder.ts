34e250d3253b9f358de4b81c0f97c83d49d8659e
dima <dima@TOMBP11.local>
2024-09-19 11:11:02 +0200

    general improvements

--- src/decoder.ts
+++ src/decoder.ts
@@ -1,27 +1,34 @@
-
 const base64RegExp = /[a-z0-9+/]{10,}/gi
 
-export function setupDecoder(textarea: HTMLTextAreaElement, container: HTMLElement ) {
+export function setupDecoder(textarea: HTMLTextAreaElement, container: HTMLElement) {
   const cleanContainer = () => {
-     container.innerHTML = ''
+    container.innerHTML = ''
   }
 
-  document.getElementById('clear')?.addEventListener('click', () => {
+  const cleanEverything = () => {
     textarea.value = ''
     cleanContainer()
-  })
+  }
+
+  document.getElementById('clear')?.addEventListener('click', cleanEverything)
 
   textarea.addEventListener('input', (e: any) => {
     const value = (e.target?.value || '').trim()
     const base64string = value.match(base64RegExp)[0]
 
-    if(!base64string) return
+    if (!base64string) return
 
     const src = 'data:image/png;base64,' + base64string
     const img = document.createElement('img')
     img.src = src
-    
+
     cleanContainer()
     container.append(img)
+  })
+
+  textarea.addEventListener('keydown', e => {
+    if (e.code === 'Escape') {
+      cleanEverything()
+    }
   })
 }
--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,20 @@ import './style.css'
 import { setupDecoder } from './decoder.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <main class="main">
     <h1>Base64 PNG Image decoder</h1>
    
-    <div>
-      <label for="textarea">Paste data here</label>
+    <div class="stack">
+      <label for="textarea">Paste base64 string here:</label>
       <button id="clear">Clear</button>
     </div>
-    <textarea id="textarea" rows="10"></textarea>
+    <textarea id="textarea" rows="5"></textarea>
 
-    <div id="image-container"></div>
-  </div>
+    <div id="image-container" class="image"></div>
+  </main>
 `
 
-setupDecoder(document.querySelector<HTMLTextAreaElement>('#textarea')!,
-document.getElementById('image-container')!)
+setupDecoder(
+  document.querySelector<HTMLTextAreaElement>('#textarea')!,
+  document.getElementById('image-container')!
+)
